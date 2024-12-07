05717205c624cc5d85fc53e546e9df1a298b21a4
Allen <lcf7891@gmail.com>
2024-12-07 22:35:44 +0800

    update 354

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -14,14 +14,14 @@
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
-      INSERT INTO	"USER" (name, email, role)
-      VALUES
-        ('李燕容', 'lee2000@hexschooltest.io','USER'),
-        ('王小明', 'wXlTq@hexschooltest.io','USER'),
-        ('肌肉棒子', 'muscle@hexschooltest.io','USER'),
-        ('好野人', 'richman@hexschooltest.io','USER'),
-        ('Q太郎', 'starplatinum@hexschooltest.io','USER'),
-        ('透明人', 'opacity0@hexschooltest.io','USER');
+    INSERT INTO	"USER" (name, email, role)
+    VALUES
+      ('李燕容', 'lee2000@hexschooltest.io','USER'),
+      ('王小明', 'wXlTq@hexschooltest.io','USER'),
+      ('肌肉棒子', 'muscle@hexschooltest.io','USER'),
+      ('好野人', 'richman@hexschooltest.io','USER'),
+      ('Q太郎', 'starplatinum@hexschooltest.io','USER'),
+      ('透明人', 'opacity0@hexschooltest.io','USER');
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
     UPDATE "USER"
@@ -351,6 +351,6 @@
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
     SELECT
-      COUNT(DISTINCT user_id) AS	預約會員人數
+      COUNT(DISTINCT user_id) AS 預約會員人數
     FROM "COURSE_BOOKING" 
     WHERE (created_at BETWEEN '2024-11-01' AND '2024-11-30') AND status != '課程已取消';
